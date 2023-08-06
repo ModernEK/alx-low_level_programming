47d57044ba79616ebf214cbe66f2017a35d3a9a7
Dodzy <caster5445@gmail.com>
2023-08-06 10:47:13 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,18 +10,21 @@ void print_alphabet_x10(void)
 {
     char ch;
     int i;
-
     i = 0;
 
     while (i < 10)
     {
         ch = 'a';
-        while (ch <= 'z')
+
+	while (ch <= 'z')
         {
             _putchar(ch);
-            ch++;
+
+	    ch++;
         }
-        _putchar('\n');
-        i++;
+
+	_putchar('\n');
+
+	i++;
     }
 }
